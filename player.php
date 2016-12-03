0f5e5a529e1fb301102eab3f1e086bf9f078c7a0
iShinta <ho.minhtri.net@gmail.com>
2016-12-03 11:46:02 -0600

    Player test 5

--- player.php
+++ player.php
@@ -1,3 +1,3 @@
-<audio src="music/soundtrack/01 - Opening.mp3" style="width: 220px; display: block;" autoplay>
+<audio src="music/soundtrack/01 - Opening.mp3" style="width: 220px; display: block;" controls="controls" autoplay>
     <p>Fallback content goes here.</p>
 </audio>
