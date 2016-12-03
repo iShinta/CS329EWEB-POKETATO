54d4f4b0553e7b0ff7b9fd09ddad82251cd66290
iShinta <ho.minhtri.net@gmail.com>
2016-12-03 12:12:14 -0600

    Carroussel 0.1

---
+++ caroussel.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+
+<html>
+<head>
+	<title>Travel Show</title>
+	<link rel="stylesheet" type="text/css" href="travel_show.css" />
+</head>
+
+<body>
+	<h1>Travel Show</h1>
+	<script type="text/javascript">
+	<!--
+		var images = new Array();
+		images[0] = "001bulbasaur.png";
+		images[1] = "016pidgey";
+		images[2] = "028sandslash.png";
+		images[3] = "037vulpix";
+		images[4] = "040wigglytuff.png";
+		images[5] = "043oddish.png";
+		images[6] = "130gyarados.png";
+		images[7] = "133eevee.png";
+		images[8] = "143snorlax.png";
+		images[9] = "146moltres.png";
+		var captions = new Array();
+		captions[0] = "agra_fort";
+		captions[1] = "ajanta_ellora";
+		captions[2] = "akshardham_temple";
+		captions[3] = "char_minar";
+		captions[4] = "hawa_mahal";
+		captions[5] = "mumbai";
+		captions[6] = "mysore_palace";
+		captions[7] = "sun_temple";
+		captions[8] = "taj_mahal";
+		captions[9] = "victoria_memorial";
+		index = 0;
+		document.writeln("<p id=\"pic\">")
+		document.writeln("<img src=\"img\pokemon-pics\" + images[index] + "\" title=\"Travel\" alt=\"" + captions[index] + "\" /><br />" + captions[index]);
+		document.writeln("</p>");
+
+		setInterval(function(){
+			if(++index == images.length){
+				index = 0;
+			}
+			document.getElementById("pic").innerHTML = "<img src=\"img\pokemon-pics\" + images[index] + "\" title=\"Travel\" alt=\"" + captions[index] + "\" /><br />" + captions[index];
+		}, 3000);
+
+
+	-->
+	</script>
+</body>
+</html>
