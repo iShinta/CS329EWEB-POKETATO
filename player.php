266c818dbda8594822d59f82952b98990dd35efe
iShinta <ho.minhtri.net@gmail.com>
2016-12-03 11:49:32 -0600

    Player inline

--- player.php
+++ player.php
@@ -1,3 +1,5 @@
-<audio src="music/soundtrack/01 - Opening.mp3" style="width: 220px; display: block;" controls="controls" autoplay="autoplay">
-    <p>Fallback content goes here.</p>
-</audio>
+<div style="display: inline-block">
+  <audio src="music/soundtrack/01 - Opening.mp3" style="width: 220px; display: block;" controls="controls" autoplay="autoplay">
+      <p>Fallback content goes here.</p>
+  </audio>
+<div>
