dbc6a5502fa2b7fc4b26e910b35ef14b9de0a43b
iShinta <ho.minhtri.net@gmail.com>
2016-12-03 11:56:20 -0600

    Player inline 5

--- player.php
+++ player.php
@@ -1,5 +1,6 @@
 <div style="display: inline-block;">
-  <audio src="music/soundtrack/01 - Opening.mp3" style="width: 220px; display: block;" controls="controls" autoplay="autoplay">
+  <!-- controls="controls" -->
+  <audio src="music/soundtrack/01 - Opening.mp3" style="width: 220px; display: block;" autoplay="autoplay">
       <p>Fallback content goes here.</p>
   </audio>
 </div>
