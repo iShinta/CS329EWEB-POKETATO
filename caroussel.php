a84228ca7ad4edb4683309a9238c6ead8de947c7
iShinta <ho.minhtri.net@gmail.com>
2016-12-07 11:43:44 -0600

    Select

--- caroussel.php
+++ caroussel.php
@@ -20,39 +20,31 @@
       	<!--
       		var images = new Array();
       		images[0] = "001bulbasaur.png";
-      		images[1] = "002ivysaur.png";
-      		images[2] = "003venusaur.png";
-      		images[3] = "004charmander.png";
-      		images[4] = "005charmander.png";
-      		images[5] = "006charizard.png";
-      		images[6] = "007squirtle.png";
-      		images[7] = "008wartortle.png";
-      		images[8] = "009blastoise.png";
-      		images[9] = "010caterpie.png";
       		var captions = new Array();
       		captions[0] = "001bulbasaur";
-      		captions[1] = "002ivysaur";
-      		captions[2] = "003venusaur";
-      		captions[3] = "004charmander";
-      		captions[4] = "005charmander";
-      		captions[5] = "006charizard";
-      		captions[6] = "007squirtle";
-      		captions[7] = "008wartortle";
-      		captions[8] = "009blastoise";
-      		captions[9] = "010caterpie";
       		index = 0;
       		document.writeln("<p id=\"pic\">")
       		document.writeln("<img src=\"img/pokemon-pics/" + images[index] + "\" title=\"Travel\" alt=\"" + captions[index] + "\" /><br />" + captions[index]);
       		document.writeln("</p>");
 
+          var xhr;
+          if (window.ActiveXObject) {
+            xhr = new ActiveXObject ("Microsoft.XMLHTTP");
+          }else if (window.XMLHttpRequest) {
+            xhr = new XMLHttpRequest ();
+          }
+
       		setInterval(function(){
-      			if(++index == images.length){
-      				index = 0;
-      			}
-      			document.getElementById("pic").innerHTML = "<img src=\"img/pokemon-pics/" + images[index] + "\" title=\"Travel\" alt=\"" + captions[index] + "\" /><br />" + captions[index];
+            var url = "rndpic.php";
+            xhr.open("GET", url, true);
+            xhr.onreadystatechange = function() {
+              if ((xhr.readyState == 4) && (xhr.status == 200)) {
+                var response = xhr.responseText.split(",");
+                document.getElementById("pic").innerHTML = "<img src=\"" + response[0] + "\" title=\"Travel\" alt=\"" + response[1] + "\" /><br />" + response[1];
+              }
+          	};
+            xhr.send(null);
       		}, 3000);
-
-
       	-->
       	</script>
       </div>
---
+++ select.php
@@ -0,0 +1,24 @@
+<?php
+  $servername = "fall-2016.cs.utexas.edu";
+  $username = "kevintn";
+  $password = "RRtcQ26KjE";
+  $dbname = "cs329e_kevintn";
+  $port = "3306";
+
+  $connect = mysqli_connect ($servername, $username, $password, $dbname);
+
+  if (empty($connect))
+  {
+    die("mysqli_connect failed: " . mysqli_connect_error());
+  }
+
+  //print "Connected to ". mysqli_get_host_info($connect) . "<br /><br />\n";
+
+  $table = "pokedex";
+  $numPokemon = rand(1, 151);
+  $result = mysqli_query($connect, "SELECT * FROM $table WHERE id == $numPokemon LIMIT 1");
+
+  while($row = $result->fetch_row()){
+    echo $row[2].",".$row[1];
+  }
+?>
