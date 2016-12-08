ac0099ae3cd8a49ba03dad9f84ca06e93cfaa587
iShinta <ho.minhtri.net@gmail.com>
2016-12-07 19:22:13 -0600

    News

--- index.php
+++ index.php
@@ -25,13 +25,32 @@
 				</div>
 				<div class="news">
 					<h1>News</h1>
-          <p>Pokémon GO has been toying with spawns from the very beginning, first starting out slow by switching nests every so often, but now they’ve evolved, no pun intended. We had a Halloween event that blanketed the world in very specific Pokémon, and we now have Pokémon super-nests popping up to help promote the new daily quest system.
-          Now, Niantic is doing something even more interesting. In what might be a preview of new events to come, they’re spawning Lapras (Laprases?) on the coastlines of the Iwate Prefecture, Miyagi Prefecture, and Fukushima Prefecture, areas affected by the 2011 Tsunami.</p>
+          <p>In today's #PokemonGenerations, Ghetsis makes one last attempt to take over Unova, but a hero arrives to stop him! </p>
 				</div>
 				<div class="player">
 					<h1>Featured Video</h1>
 					<iframe width="560" height="315" src="https://www.youtube.com/embed/5s8g8NCJyvs" frameborder="0" allowfullscreen></iframe>
 				</div>
+        <div>
+          <h1>Pokemon Go new ventures</h1>
+            <p>Pokémon GO has been toying with spawns from the very beginning, first starting out slow by switching nests every so often, but now they’ve evolved, no pun intended. We had a Halloween event that blanketed the world in very specific Pokémon, and we now have Pokémon super-nests popping up to help promote the new daily quest system.</p>
+
+            <p>Now, Niantic is doing something even more interesting. In what might be a preview of new events to come, they’re spawning Lapras (Laprases?) on the coastlines of the Iwate Prefecture, Miyagi Prefecture, and Fukushima Prefecture, areas affected by the 2011 Tsunami.</p>
+
+            <p>The goal is to increase tourism in those struggling areas, and hey, if you want to buy a  few hundred Pokéballs to catch all the Lapras while you’re there, no one’s going to stop you. It’s a nice gesture, and the rest of the world is exceedingly jealous, as Lapras is one of the most popular, most powerful, but hardest to find Pokémon in the game, unless you’ve been lucky with spawns or hatching 10 km eggs.</p>
+
+            <p>The event runs until November 23rd, so that’s a bit of late notice for you to plan a trip to Japan, but hopefully those nearby will be able to take advantage. Given the game’s popularity in Japan, and the popularity of Lapras in the game, I’m guessing this will be a hit.</p>
+
+            <p>It is interesting, however, to see Niantic playing around with the idea of very, very targeted spawns, something we have not seen them do to date, as most of their spawn finagling has been global. The only “region” spawns are the four region-specific Pokémon that spawn in NA, EU, Asia and Oceana. But this, limiting spawns of specific Pokémon to very, very specific areas, is something new.</p>
+
+            <p>I have to believe that in addition to being a “for a good cause” event, they’re testing out the concept for future events like this, ones that may have less to do with charity, and may serve instead to be big promotional vehicles to get people out to catch something super rare, like the Legendary Birds, Mew or Mewtwo.</p>
+
+            <p>The original commercial for Pokémon GO showed a bunch of people attempting to catch Mewtwo in Times Square, but obviously nothing like that has ever happened in actuality yet. But that doesn’t meant it couldn’t. It’s easy to imagine Niantic using this same sort of targeting to spawn legendary Pokémon in specific locations, inspiring road trips or mass meet-ups unlike anything the game has seen yet.</p>
+
+            <p>Would they really do it with one spawn? After all, it would be a bit goofy to have an area full of Mewtwos like they’re doing with Lapras here. It’s intriguing to consider the possibilities.</p>
+
+            <p>But yeah, if you want a guaranteed army of Lapras in the next two weeks here, it’s time to head out to Japan and frequent these recovering regions.</p>
+        </div>
         <div class="caroussel">
           <h1 style="text-align: center;">Pokemons Caroussel!</h1>
         	<script type="text/javascript">
@@ -66,28 +85,7 @@
         	-->
         	</script>
         </div>
-        The goal is to increase tourism in those struggling areas, and hey, if you want to buy a  few hundred Pokéballs to catch all the Lapras while you’re there, no one’s going to stop you. It’s a nice gesture, and the rest of the world is exceedingly jealous, as Lapras is one of the most popular, most powerful, but hardest to find Pokémon in the game, unless you’ve been lucky with spawns or hatching 10 km eggs.
-
-        The event runs until November 23rd, so that’s a bit of late notice for you to plan a trip to Japan, but hopefully those nearby will be able to take advantage. Given the game’s popularity in Japan, and the popularity of Lapras in the game, I’m guessing this will be a hit.
-
-        Pokémon GO has been toying with spawns from the very beginning, first starting out slow by switching nests every so often, but now they’ve evolved, no pun intended. We had a Halloween event that blanketed the world in very specific Pokémon, and we now have Pokémon super-nests popping up to help promote the new daily quest system.
-
-        Now, Niantic is doing something even more interesting. In what might be a preview of new events to come, they’re spawning Lapras (Laprases?) on the coastlines of the Iwate Prefecture, Miyagi Prefecture, and Fukushima Prefecture, areas affected by the 2011 Tsunami.
-
-        The goal is to increase tourism in those struggling areas, and hey, if you want to buy a  few hundred Pokéballs to catch all the Lapras while you’re there, no one’s going to stop you. It’s a nice gesture, and the rest of the world is exceedingly jealous, as Lapras is one of the most popular, most powerful, but hardest to find Pokémon in the game, unless you’ve been lucky with spawns or hatching 10 km eggs.
-
-        The event runs until November 23rd, so that’s a bit of late notice for you to plan a trip to Japan, but hopefully those nearby will be able to take advantage. Given the game’s popularity in Japan, and the popularity of Lapras in the game, I’m guessing this will be a hit.
-
-        It is interesting, however, to see Niantic playing around with the idea of very, very targeted spawns, something we have not seen them do to date, as most of their spawn finagling has been global. The only “region” spawns are the four region-specific Pokémon that spawn in NA, EU, Asia and Oceana. But this, limiting spawns of specific Pokémon to very, very specific areas, is something new.
-
-        I have to believe that in addition to being a “for a good cause” event, they’re testing out the concept for future events like this, ones that may have less to do with charity, and may serve instead to be big promotional vehicles to get people out to catch something super rare, like the Legendary Birds, Mew or Mewtwo.
-
-        The original commercial for Pokémon GO showed a bunch of people attempting to catch Mewtwo in Times Square, but obviously nothing like that has ever happened in actuality yet. But that doesn’t meant it couldn’t. It’s easy to imagine Niantic using this same sort of targeting to spawn legendary Pokémon in specific locations, inspiring road trips or mass meet-ups unlike anything the game has seen yet.
-
-        Would they really do it with one spawn? After all, it would be a bit goofy to have an area full of Mewtwos like they’re doing with Lapras here. It’s intriguing to consider the possibilities.
-
-        But yeah, if you want a guaranteed army of Lapras in the next two weeks here, it’s time to head out to Japan and frequent these recovering regions.
-			</div>
+      </div>
 			<div class="right_content">
         5 days left until #PokemonSunMoon, US Trainers! Which Pokémon do you want on your team? http://bit.ly/1OnFndH
 				<div class="ad">
